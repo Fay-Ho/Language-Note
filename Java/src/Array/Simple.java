a1bf3f28a23806db00a732af02007c5800d02c25
Fay-Ho <fay.hepengfei@gmail.com>
2021-01-16 15:13:54 +0800

    no message

---
+++ Java/src/Array/Simple.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Simple {
+    public static void main(String[] args) {
+        Model model1 = new Model("a");
+        Model model2 = new Model("b");
+        Model model3 = new Model("c");
+        List<Model> list = Arrays.asList(model1, model2, model3);
+        list.stream().filter(model -> model.key.equals("b")).findFirst().ifPresent(model -> System.out.println(model.key));
+    }
+}
+
+final class Model {
+
+    String key;
+
+    Model(String key) {
+        this.key = key;
+    }
+}
