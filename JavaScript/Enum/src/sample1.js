69844bde39c01bcbfca4e6408c06bfaf8f0bbab5
Fay-Ho <fay.hepengfei@gmail.com>
2021-11-30 02:16:22 +0800

    Update Note

---
+++ JavaScript/Enum/src/sample1.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const Color = {
+    Red: "The color is red.",
+    Orange: "The color is orange.",
+    Yellow: "The color is yellow.",
+    Green: "The color is green.",
+    Cyan: "The color is cyan.",
+    Blue: "The color is blue.",
+    Purple: "The color is purple."
+}
+
+function main() {
+    console.log(Color.Blue);
+}
+
+main();
