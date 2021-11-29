38edf2cc47cb095cc31ab4cd72e82056e99823ab
Fay-Ho <fay.hepengfei@gmail.com>
2021-11-30 02:37:25 +0800

    Update Note

---
+++ Go/Enum/src/sample1.go
@@ -0,0 +1,16 @@
+package main
+
+type Color string
+const (
+	Red Color = "The color is red."
+	Orange Color = "The color is orange."
+	Yellow Color = "The color is yellow."
+	Green Color = "The color is green."
+	Cyan Color = "The color is cyan."
+	Blue Color = "The color is blue."
+	Purple Color = "The color is purple."
+)
+
+func main() {
+	println(Blue)
+}
